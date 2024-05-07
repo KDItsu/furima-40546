22c947f7d0ca38fa102fd737c5ec07ef9d0af19a
KDItsu <ksmhkssp4567@gmail.com>
2024-05-07 17:19:16 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new#入力ページ
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
+
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,11 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :item_condition_id, numericality: { other_than: 1 }
-  validates :shipping_charge_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :shipping_date_id, numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1, message: "Category can't be blank" }
+  validates :item_condition_id, numericality: { other_than: 1, message: "Sales status can't be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1, message: "Shipping fee status can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "Prefecture can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 1, message: "Scheduled delivery can't be blank" }
   belongs_to :user
   has_one :order
   has_one_attached :image
