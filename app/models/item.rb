52f4489c1b78d0076b93f7b43a68e7024a0f30f8
KDItsu <ksmhkssp4567@gmail.com>
2024-05-07 12:30:58 +0900

    ActiveHashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :item_condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_date_id, numericality: { other_than: 1 }
   belongs_to :user
   has_one :order
   has_one_attached :image
