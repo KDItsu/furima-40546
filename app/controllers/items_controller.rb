f13d7a26cc5728715c73ec421da6225453098b15
KDItsu <ksmhkssp4567@gmail.com>
2024-05-11 11:48:57 +0900

    edit,updateアクションを定義する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:edit, :new]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
   end
@@ -18,10 +19,28 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+     if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def set_item
     @item = Item.find(params[:id])
   end
 
-  private
 
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :item_condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
