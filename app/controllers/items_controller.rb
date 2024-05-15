febc3d045f6250814e2ad5b33d010dd09ea95f18
KDItsu <ksmhkssp4567@gmail.com>
2024-05-15 12:02:53 +0900

    条件分岐
    ログイン状態の場合でも、自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移すること（商品情報編集機能）。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id
+    if current_user.id == @item.user_id && @item.order.present?
       redirect_to root_path
-    end
+    end 
   end
 
   def update
