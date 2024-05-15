e8ea1cadcaec8b60cdf8db2ec43515391ffcf885
KDItsu <ksmhkssp4567@gmail.com>
2024-05-15 12:01:36 +0900

    コントローラーの作成
    ・マイグレーションテーブルへの保存
    ・コントローラーでgonを使用して公開鍵を代入しよう
    ・実装条件分岐
    ・pay.jpの決済処理を記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,40 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
+    if current_user.id == @item.user_id || @item.order.present? 
+      redirect_to root_path
+    end
+  end
+
+  def create
+    
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
